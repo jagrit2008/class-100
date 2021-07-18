fa4d4e31f9e8eddc7d644f3fff77a835b744911d
jagrit2008 <70403360+jagrit2008@users.noreply.github.com>
2021-07-18 22:56:47 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,14 @@
+class ATM:
+    def __init__(self,cashWithdrawl,BalanceEnquiry):
+        self.cashWithdrawl=cashWithdrawl
+        self.BalanceEnquiry=BalanceEnquiry
+    
+    def cashWithdrawl(self):
+       input('enter the amount: ')
+       input('enter card number')
+       input('enter the pin: ')
+
+    def BalanceEnquiry(self):
+        print('₹10000')
+
+
